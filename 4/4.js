5ac2bc3a3f9239e31b0a2a7b318fe205e5f145aa
thekaleidoscope <yadhuksp@gmail.com>
2018-09-18 20:01:52 +0530

    Till Exp4

---
+++ 4/4.js
@@ -0,0 +1,28 @@
+function ValidationEvent() {
+  // Validate Name
+  var name = document.getElementById("name").value;
+  var pass = document.getElementById("pass").value;
+  var email = document.getElementById("email").value;
+  var f = 0;
+  if (/^(?![a-z0-9]{5,})/.test(name)) {
+    f = 1;
+    document.getElementById("errormsg").innerHTML =
+      '<p class="mer">Invalid Name</p>';
+    // console.log("WTF!");
+  }
+
+  if (/^(?![a-z0-9]{8,})/.test(pass)) {
+    f = 1;
+    document.getElementById("errormsg2").innerHTML =
+      '<p class="mer">Invalid Password</p>';
+  }
+  //
+  //
+  if (/^(?![a-z0-9]{5,}@[+a-z\.com])/.test(email)) {
+    f = 1;
+    document.getElementById("errormsg3").innerHTML =
+      '<p class="mer">Invalid Email</p>';
+  }
+  if (f === 1) return false;
+  else return true;
+}
